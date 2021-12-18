3c3509d078e5238903631c0d7602ffd799fa006c
Albert Slyvester Duro <slyboydon1@gmail.com>
2021-12-18 18:13:09 +0100

    initial commit

---
+++ html and css crash course/app.js
@@ -0,0 +1,20 @@
+
+// let x = 4
+
+// if (x > 10) {
+//     console.log("greater than 10")
+// } else {
+//     console.log("lesser than 10")
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+const ralph = {
+    name: "Ralph",
+    age: 54,
+    greet: () => {
+        console.log('hello there')
+    }
+}
